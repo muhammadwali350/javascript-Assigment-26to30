12c60254d01e1463f7c4d50301dad3d46a209473
mohammadwalikhan <mohammadwalikhan1@github.com>
2021-05-27 09:25:28 +0500

    assigment completed

---
+++ app.js
@@ -0,0 +1,88 @@
+// TASK 1
+
+// var number = +prompt("enter a number");
+// var roundoffNum = Math.round(number);
+// var ceilNum = Math.ceil(number);
+// var floorNum = Math.floor(number);
+// if(number > 0 ){
+// document.write("number that you enter is :" + number + "</br>" + "round off number is :" + roundoffNum + "</br>" + "ciel number is :" + ceilNum + "</br>" +  "floor Number is :" + floorNum)
+
+// }
+// else{
+//     alert ("please enter a number")
+// }
+
+
+// TASK 2
+
+// var number = +prompt("enter a number");
+// var roundoffNum = Math.round(number);
+// var ceilNum = Math.ceil(number);
+// var floorNum = Math.floor(number);
+// if(number < 0 ){
+// document.write("number that you enter is :" + " " + number + "</br>" + "round off number is :" + " " + roundoffNum + "</br>" + "ciel number is :" + " " + ceilNum + "</br>" +  "floor Number is :" + " " + floorNum)
+
+// }
+// else{
+//     alert ("please enter a number")
+// }
+
+
+// TASK 3
+
+// var number = +prompt("enter a number");
+// var newNumber = Math.abs(number);
+// alert("The absolute number is : "  + " " + newNumber);
+
+
+// TASK 4 
+
+// var number;
+// var newNumber = Math.random()*6+1;
+// console.log(newNumber.toFixed(0));
+
+
+// TASK 5
+
+// var head = 1;
+// var tail = 2;
+
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss)
+// if(floor === 0){
+//     var playerOne = prompt("enter player one");
+//     document.write("0 <br> Random Coin Value: Head")
+// } else if(floor === 1){
+//     var playerTwo = prompt("enter player two");
+//     document.write("1 <br> Random Coin Value: Tails")
+// }
+
+
+// TASK 6
+
+// var randomNum = Math.random(Math.round())*100
+// document.write("The random number between 1 and  100 is :" + " " + randomNum.toFixed());
+
+
+// TASK 7
+
+// var weight = +prompt("Enter your weight in kilograms");
+// var output = parseInt(weight);
+// if(weight > 0  ){
+
+// document.write("your weight is :" + " " + output + "kgs")
+
+// }
+
+
+// TASK 8   
+
+// var number = +prompt("Enter a random number between 1 to 10")
+// var newNumber = Math.random(number.toFixed(0)) * 10;
+// if(number === 7){
+// alert("congratulations! the number you entered is matched.")
+// }
+
+// else{
+//     alert("try agin later!")
+// }
